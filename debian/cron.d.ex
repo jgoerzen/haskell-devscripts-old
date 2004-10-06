1a9ecf7aa835ec97232b94e5c4f608dc269ecbcb
John Goerzen <jgoerzen@complete.org>
2004-10-06 13:28:56 -0500

    Working on Debian scripts
    Keywords:
    
    (jgoerzen@complete.org--projects/haskell-devscripts--head--1.0--patch-4)
    
    darcs-hash:20041006182856-c2a52-04bcd903d3aef552492de4d131abe30e09b322be.gz

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the haskell-devscripts package
-#
-0 4	* * *	root	haskell-devscripts_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(haskell-devscripts):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="haskell-devscripts" command="/usr/bin/haskell-devscripts"
